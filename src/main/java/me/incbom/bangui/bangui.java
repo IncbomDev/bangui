6aa086253be28b35557738913063f68e9749038b
Incbom <99501939+IncbomDev@users.noreply.github.com>
2022-03-15 07:45:29 -0400

    Update bangui.java

--- src/main/java/me/incbom/bangui/bangui.java
+++ src/main/java/me/incbom/bangui/bangui.java
@@ -1,7 +1,7 @@
 package me.incbom.bangui;
 
-import me.kodysimpson.bangui.commands.BanGUICommand;
-import me.kodysimpson.bangui.listener.BanInventoryListener;
+import me.incbom.bangui.commands.BanGUICommand;
+import me.incbom.bangui.listener.BanInventoryListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BanGUI extends JavaPlugin {
